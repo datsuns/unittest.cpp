a12a5bd28b8e80a2ab9fb6d40a8d9f64d2a9087d
datsuns <the.skeleton7@gmail.com>
2016-06-09 08:31:31 +0900

    add simple test

--- product/product.h
+++ product/product.h
@@ -1,6 +1,8 @@
 #ifndef PRODUCT_H_INCLUDED
 #define PRODUCT_H_INCLUDED
 
+class Product{
+};
 
 
 #endif
--- test/test_product.cpp
+++ test/test_product.cpp
@@ -1,1 +1,20 @@
 #include <gmock/gmock.h>
+#include "product.h"
+
+using testing::Eq;
+using testing::Ne;
+using testing::NotNull;
+
+class TestProduct : public testing::Test{
+  protected:
+    virtual void SetUp() {
+    }
+
+    virtual void TearDown() {
+    }
+};
+
+TEST_F(TestProduct, hello) {
+  EXPECT_THAT(0, Eq(1));
+}
+
